bfd4a5df9601286b9c80bb7071b66838c214039e
Matt Harrington <mharr1729@gmail.com>
2018-02-02 13:02:14 -0500

    adding the files to the new project

---
+++ src/src/GenerateReports.java
@@ -0,0 +1,39 @@
+public class GenerateReports
+{
+
+    // default constructor
+    public GenerateReports()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // end of GenerateReports class
---
+++ src/src/TakeOut.java
@@ -0,0 +1,44 @@
+public class TakeOut extends OrderInfo
+{
+
+    // creating variables
+    final int typeID = 96700477;
+    int boxAmount = 0;
+    int phoneNumber = 0;
+
+
+    // default constructor
+    public TakeOut()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
